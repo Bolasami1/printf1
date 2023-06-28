ca72e58a3e12689d38f546a5a6ea09d2640347c1
Bolanle <bolanlesamuell@gmail.com>
2023-06-28 13:11:56 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -9,54 +9,40 @@
   *Return: length of the formatted output string.
   */
 
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0) {
-		int i;
-		for (i = 0; i < *buff_ind; i++) {
-			_putchar(buffer[i]);
-		}
-	}
-	*buff_ind = 0;
-}
-
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list args;
+	flags_t flags = {0, 0, 0};
 
-	if (format == NULL)
+	register int count = 0;
+
+	va_start(args, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++) {
-		if (format[i] != '%') {
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE) {
-				print_buffer(buffer, &buff_ind);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
 			}
-			printed_chars++;
-		} else {
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(args, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+	_putchar(-1);
+	va_end(args);
+	return (count);
 }
